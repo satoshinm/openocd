114b0656d82ec887d9b7451d1735cd3de00991b6
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-12-18 18:22:37 -0800

    Revert "Fix compile errors"
    This reverts commit 6ac33a8423d24af9579b2022c17163ec97a1ad08.

--- src/helper/command.c
+++ src/helper/command.c
@@ -1456,7 +1456,6 @@ COMMAND_HELPER(handle_command_parse_bool, bool *out, const char *label)
 			}
 			/* fall through */
 		}
-		/* Falls through */
 		case 0:
 			LOG_INFO("%s is %s", label, *out ? "enabled" : "disabled");
 			break;
